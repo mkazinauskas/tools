84146e28ee871c7d9672b683c9d31c5ff60506fb
m.kazinauskas@gmail.com <m.kazinauskas@gmail.com>
2023-01-08 18:07:11 +0200

    Google analytics integration

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
-while [ true ]
-do
-	docker kill tools_getbestfree
-	docker run --name tools_getbestfree -d --rm --volume="$PWD:/srv/jekyll:Z" --publish 4000:4000 jekyll/jekyll:3 jekyll serve
-  read  -n 1 -p "Hit ENTER to reload, ctrl+c - stop:" ignored
-  docker kill tools_getbestfree
-done
+#!/bin/bash
+
+docker kill tools_getbestfree
+docker rm tools_getbestfree
+
+docker run \
+  --name tools_getbestfree \
+  -d --rm \
+  --volume="$PWD:/srv/jekyll:Z" \
+  --log-driver=journald \
+  --publish 4000:4000 \
+  jekyll/jekyll:3 \
+  jekyll serve
+
+trap 'echo "Exiting application"; docker kill tools_getbestfree; exit;' SIGINT
+
+journalctl CONTAINER_NAME=tools_getbestfree -f
